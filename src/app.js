95dcd38084916592798be3121b963a675d432aa2
giorgiorindonone <giorgio.rindonone@gmail.com>
2021-04-22 03:06:01 +0200

    update_menu_footer

--- src/app.js
+++ src/app.js
@@ -1,61 +1,36 @@
-//
-// Vue.component('ul', {
-//   props: ['item'],
-//
-//   template:
-//   <p>Hello</p>
-//
-// });
-//
-//
-// var app = new Vue(
-//
-//   {
-//   el: '#app',
-//
-//   data: {
-//       items: [
-//         { },
-//         { title: 'Title 2', description: 'Description for tab 2.' },
-//         { title: 'Title 3', description: 'Description for tab 3.' },
-//       ]
-//     }
-//   }
-// });
-// Vue.component('voiceMenu', {
-//   props: ['menu'],
-//   template: '<li>{{ menu.menuVoice }}</li>'
-// })
-// var app = new Vue({
-//   el: '#app',
-//   data: {
-//     menuList: [
-//       { id:1, menuVoice: 'Home' },
-//       { id:2, menuVoice: 'Work' },
-//       { id:3, menuVoice: 'Articles' },
-//       { id:4, menuVoice: "Let's Talk" }
-//     ]
-//   },
-//   methods: {
-//     hideShow: function() {
-//       var hamburger = '#menuHamb';
-//       hamburger.fadeOut()
-//     }
-//   }
-// })
 
-Vue.component('menu-item', {
-  props: ['menu'],
-  template: '<li><a href="{{menuVoices.link}}">{{menu.text}}</a></li>'
-})
-var app7 = new Vue({
-  el: '#app-menu__header',
+
+var app = new Vue(
+  {
+  el: "#app-menu__header",
   data: {
     menuVoices: [
-      { id: 0, text: 'Home', link:"https://www.worldtimebuddy.com/" },
-      { id: 1, text: 'Work', link:"https://stackoverflow.com/questions/33731939/vue-js-toggle-class-on-click" },
-      { id: 2, text: 'Articles', link:"https://somiibo.com/pricing/" },
-      { id: 3, text: "Let's Talk", link:"https://www.worldtimebuddy.com/" }
-    ]
+      {id:'1', text: 'Home', link:"https://vuejs.org/v2/guide/index.html"},
+      {id:'2', text: 'Work', link:"https://vuejs.org/v2/guide/index.html"},
+      {id:'3', text: 'Articles', link:"https://vuejs.org/v2/guide/index.html"},
+      {id:'4', text: "Let's Talk", link:"https://vuejs.org/v2/guide/index.html"},
+    ],
+    menuHamb: false
+  }
+})
+
+
+var app2 = new Vue(
+  {
+  el: "#app-footer",
+  data: {
+    rightsVoices: [
+      {text: "© Copyright 2012 - 2021"},
+      {text: "Avada Theme by <strong>Theme Fusion</strong>"},
+      {text: "All Rights Reserved"},
+      {text: "Powered by <strong>WordPress</strong>"},
+    ],
+    socialIcons: [
+      {name: "facebook", link:"https://vuejs.org/v2/guide/index.html", icon:"fab fa-facebook-f"},
+      {name: "twitter", link:"https://vuejs.org/v2/guide/index.html", icon:"fab fa-twitter"},
+      {name: "instagram", link:"https://vuejs.org/v2/guide/index.html", icon:"fab fa-instagram"},
+      {name: "youtube", link:"https://vuejs.org/v2/guide/index.html", icon:"fab fa-youtube"}
+    ],
+    imgLogo: {src: "img/avada-footer-logo.png"}
   }
 })
